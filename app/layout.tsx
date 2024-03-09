f7c9af984d08376565e84f918d0e6cecf88ca866
Thabani Takwena <thabanidev@gmail.com>
2024-03-10 00:25:08 +0200

    tweaking file structure and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,31 +13,30 @@ const fontSans = FontSans({
 export const metadata: Metadata = {
   title: "One Month",
   description: "A monthly budgeting app, and birthday present for my mom.",
-  applicationName: "One Month",
-  keywords: ["budgeting", "money", "finance"],
+  keywords: ["budgeting", "money", "finance", "personal finance", "budget", "monthly budget", "monthly budgeting", "budget app", "money app", "finance app", "personal finance app", "budgeting app", "monthly budgeting app", "budgeting app for mom", "monthly budgeting app for mom", "budget app for mom", "money app for mom", "finance app for mom", "personal finance app for mom", "budgeting app", "monthly budgeting app", "budget app", "money app", "finance app", "personal finance app"],
   creator: "Thabani Takwena",
   manifest: "/site.webmanifest",
   icons: [
     {
       rel: "shortcut icon",
-      url: "/assets/images/logo.png",
+      url: "/images/logo.png",
     },
     {
       rel: "apple-touch-icon",
       sizes: "180x180",
-      url: "/assets/images/apple-touch-icon.png",
+      url: "/images/apple-touch-icon.png",
     },
     {
       rel: "icon",
       type: "image/png",
       sizes: "32x32",
-      url: "/assets/images/favicon-32x32.png",
+      url: "/images/favicon-32x32.png",
     },
     {
       rel: "icon",
       type: "image/png",
       sizes: "16x16",
-      url: "/assets/images/favicon-16x16.png",
+      url: "/images/favicon-16x16.png",
     },
   ],
 };
@@ -54,9 +53,6 @@ export default function RootLayout({
           layout: {
             socialButtonsPlacement: 'bottom',
             socialButtonsVariant: 'auto',
-            termsPageUrl: 'https://clerk.com/terms',
-            logoImageUrl: '/assets/images/logo.png',
-            logoPlacement: 'outside'
           },
           variables: {
             colorPrimary: "#173d4c",
